bd007d188ff467f3ca781ea93bcfd4d871dcf5c4
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2017-08-11 14:43:07 +0200

    Add Term getters/setters

--- src/ValueObjects/Term.php
+++ src/ValueObjects/Term.php
@@ -25,4 +25,55 @@ class Term
      */
     protected $domain;
 
+    /**
+     * @return string
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Term
+     */
+    public function setId($id) {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel() {
+        return $this->label;
+    }
+
+    /**
+     * @param string $label
+     * @return Term
+     */
+    public function setLabel($label) {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDomain() {
+        return $this->domain;
+    }
+
+    /**
+     * @param string $domain
+     * @return Term
+     */
+    public function setDomain($domain) {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
 }
