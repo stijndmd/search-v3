5bd8c6f99e009539dc7db1357b1e254987e5a3e2
Nils Destoop <nils.destoop@gmail.com>
2017-10-16 00:32:58 +0200

    Added ID parameter

---
+++ src/Parameter/Id.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CultuurNet\SearchV3\Parameter;
+
+/**
+ * Provides a parameter to search on id.
+ */
+class Id extends AbstractParameter
+{
+
+    /**
+     * Id constructor.
+     * @param $id
+     */
+    public function __construct($id)
+    {
+        $this->value = $id;
+        $this->key = 'id';
+    }
+
+}
