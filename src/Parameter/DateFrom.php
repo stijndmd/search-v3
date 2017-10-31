0c4231cc4359704db77375b60cafc68ecf3ce90a
Stijn Swaanen <swaanenstijn@gmail.com>
2017-10-31 15:32:26 +0100

    III-2352 add dateFrom as parameter

---
+++ src/Parameter/DateFrom.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CultuurNet\SearchV3\Parameter;
+
+/**
+ * Provides a parameter to search on dateFrom.
+ */
+class DateFrom extends AbstractParameter
+{
+
+    /**
+     * DateFrom constructor.
+     * @param $dateFrom
+     */
+    public function __construct($dateFrom)
+    {
+        $this->value = $this->formatDate($dateFrom);
+        $this->key = 'dateFrom';
+    }
+
+    /**
+     * Formats the date.
+     * @param $dateFrom
+     * @return string
+     */
+    private function formatDate($dateFrom)
+    {
+        $date = new \DateTime($dateFrom);
+        return $date->format('Y-m-d H:i:s');
+    }
+}
+``
