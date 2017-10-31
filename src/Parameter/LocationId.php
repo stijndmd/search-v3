e0c606ae810ab8bd8c17986e29a9189263543fba
Stijn Swaanen <swaanenstijn@gmail.com>
2017-10-31 15:48:05 +0100

    III-2354 add locationId as parameter

---
+++ src/Parameter/LocationId.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CultuurNet\SearchV3\Parameter;
+
+/**
+ * Provides a parameter to search on locationId.
+ */
+class LocationId extends AbstractParameter
+{
+
+    /**
+     * locationId constructor.
+     * @param $locationId
+     */
+    public function __construct($locationId)
+    {
+        $this->value = $locationId;
+        $this->key = 'locationId';
+    }
+}
