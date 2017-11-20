b2f185dff4e6a99cb84e2c25dc83174a657c6034
Stijn Swaanen <swaanenstijn@gmail.com>
2017-11-20 16:11:27 +0100

    III-2369 unit tests for value objects Term

---
+++ test/ValueObjects/TermTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CultuurNet\SearchV3\Test\ValueObjects;
+
+use CultuurNet\SearchV3\ValueObjects\Term;
+
+class TermTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Term
+     */
+    protected $term;
+
+    public function setUp() {
+        $this->term = new Term();
+
+        $this->term->setId('term-id');
+        $this->term->setLabel('term-label');
+        $this->term->setDomain('term-domain');
+    }
+
+    public function testGetIdMethod()
+    {
+        $result = $this->term->getId();
+        $this->assertEquals($result, 'term-id');
+    }
+
+    public function testGetLabelMethod()
+    {
+        $result = $this->term->getLabel();
+        $this->assertEquals($result, 'term-label');
+    }
+
+    public function testGetDomainMethod()
+    {
+        $result = $this->term->getDomain();
+        $this->assertEquals($result, 'term-domain');
+    }
+}
