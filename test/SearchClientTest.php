bf42a96882d0e572588071ef9a8dbe32218422cb
Stijn Swaanen <swaanenstijn@gmail.com>
2017-11-21 15:00:49 +0100

    III-2369 unit tests for searchClient findOffers

--- test/SearchClientTest.php
+++ test/SearchClientTest.php
@@ -1,11 +1,13 @@
 <?php
 
-use CultuurNet\SearchV3\SearchClient;
-use CultuurNet\SearchV3\SearchQueryInterface;
-use CultuurNet\SearchV3\Serializer\SerializerInterface;
+namespace CultuurNet\SearchV3\Test;
+
 use Guzzle\Http\ClientInterface;
-use Guzzle\Http\Message\RequestInterface;
+use CultuurNet\SearchV3\Serializer\SerializerInterface;
+use CultuurNet\SearchV3\SearchQueryInterface;
+use CultuurNet\SearchV3\SearchClient;
 use Guzzle\Http\Message\Response;
+use Guzzle\Http\Message\RequestInterface;
 
 class SearchClientTest extends \PHPUnit_Framework_TestCase
 {
@@ -30,16 +32,19 @@ class SearchClientTest extends \PHPUnit_Framework_TestCase
         $this->searchClient = new SearchClient($this->client, $this->serializer);
     }
 
-    public function testSearchEventsMethod()
+    public function provideSearchQueryMock()
     {
-        $options = array('query' => 'asdfadsf');
-
         $searchQueryMock = $this->getMockBuilder(SearchQueryInterface::class)
             ->getMock();
         $searchQueryMock->expects($this->once())
             ->method('toArray')
             ->willReturn('asdfadsf');
 
+        return $searchQueryMock;
+    }
+
+    public function provideRequestMockup()
+    {
         $result = $this->getMockBuilder(Response::class)
             ->disableOriginalConstructor()
             ->getMock();
@@ -53,6 +58,16 @@ class SearchClientTest extends \PHPUnit_Framework_TestCase
             ->method('send')
             ->willReturn($result);
 
+        return $request;
+    }
+
+    public function testSearchEventsMethod()
+    {
+        $options = array('query' => 'asdfadsf');
+
+        $searchQueryMock = $this->provideSearchQueryMock();
+        $request = $this->provideRequestMockup();
+
         $this->serializer->expects($this->once())
             ->method('deserialize')
             ->with('test response')
@@ -65,5 +80,26 @@ class SearchClientTest extends \PHPUnit_Framework_TestCase
 
         $queryResult = $this->searchClient->searchEvents($searchQueryMock);
         $this->assertEquals('test event', $queryResult);
+    }
+
+    public function testSearchOfferMethod()
+    {
+        $options = array('query' => 'asdfadsf');
+
+        $searchQueryMock = $this->provideSearchQueryMock();
+        $request = $this->provideRequestMockup();
+
+        $this->serializer->expects($this->once())
+            ->method('deserialize')
+            ->with('test response')
+            ->willReturn('test offer');
+
+        $this->client->expects($this->once())
+            ->method('createRequest')
+            ->with('GET', 'offers', null, null, $options)
+            ->willReturn($request);
+
+        $queryResult = $this->searchClient->searchOffers($searchQueryMock);
+        $this->assertEquals('test offer', $queryResult);
     }
 }
