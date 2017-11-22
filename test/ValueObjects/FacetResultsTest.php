488fc2109df029e3ab91db19b2fbf3d4389b7ac7
Stijn Swaanen <swaanenstijn@gmail.com>
2017-11-22 15:07:28 +0100

    III-2369 unit test for valueObject FacetResults

---
+++ test/ValueObjects/FacetResultsTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace CultuurNet\SearchV3\Test\ValueObjects;
+
+use CultuurNet\SearchV3\ValueObjects\FacetResults;
+use CultuurNet\SearchV3\ValueObjects\FacetResult;
+use CultuurNet\SearchV3\ValueObjects\FacetResultItem;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\JsonDeserializationVisitor;
+
+class FacetResultsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var FacetResults
+     */
+    protected $facetResults;
+
+    protected $facetJson;
+
+    protected $visitor;
+
+    protected $context;
+
+    public function setUp()
+    {
+        $this->facetResults = new FacetResults();
+        $this->facetJson = json_decode(file_get_contents(__DIR__ . '/data/facetResults.json'), true);
+    }
+
+    public function deserializeEverything()
+    {
+        $this->visitor = $this->getMockBuilder(JsonDeserializationVisitor::class)
+            ->getMock();
+        $this->context = $this->getMockBuilder(DeserializationContext::class)
+            ->getMock();
+
+        $this->facetResults->deserializeFromJson($this->visitor, $this->facetJson, $this->context);
+    }
+
+    public function deserializeFacilitiesTestData($results)
+    {
+        $items = [];
+        foreach ($results as $value => $result) {
+            $children = isset($result['children']) ? $this->deserializeFacilitiesTestData($result['children']) : [];
+            $items[] = new FacetResultItem($value, $result['name'], $result['count'], $children);
+        }
+
+        return $items;
+    }
+
+    public function testGetFacetResultsMethod()
+    {
+        $this->facetResults->setFacetResults($this->facetJson);
+        $result = $this->facetResults->getFacetResults();
+        $this->assertEquals($result, $this->facetJson);
+    }
+
+    public function testGetFacetResultsByFieldMethod()
+    {
+        $this->deserializeEverything();
+
+        $facilities = json_decode(file_get_contents(__DIR__ . '/data/facetResultsFacilities.json'), true);
+        $facilitiesTestData[] = new FacetResult('facilities', $this->deserializeFacilitiesTestData($facilities));
+
+        $result = $this->facetResults->getFacetResultsByField('facilities');
+
+        $this->assertEquals($result, $facilitiesTestData);
+    }
+
+    public function testIteratorCurrentMethod()
+    {
+        $this->deserializeEverything();
+        $this->facetResults->current();
+    }
+
+    public function testIteratorNextMethod()
+    {
+        $this->deserializeEverything();
+        $this->facetResults->next();
+    }
+
+    public function testIteratorKeyMethod()
+    {
+        $this->deserializeEverything();
+        $this->facetResults->key();
+    }
+
+    public function testIteratorValidMethod()
+    {
+        $this->deserializeEverything();
+        $this->facetResults->valid();
+    }
+
+    public function testIteratorRewindMethod()
+    {
+        $this->deserializeEverything();
+        $this->facetResults->rewind();
+    }
+}
