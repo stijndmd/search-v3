700c3d32cf055deca30766aa57443abeb89113b8
Stijn Swaanen <swaanenstijn@gmail.com>
2017-11-24 14:05:30 +0100

    III-2353 add coordinates as parameter

---
+++ src/Parameter/Coordinates.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CultuurNet\SearchV3\Parameter;
+
+/**
+ * Provides a parameter to search on id.
+ */
+class Coordinates extends AbstractParameter
+{
+
+    /**
+     * Coordinates constructor.
+     * @param $latitude string
+     * @param $longitude string
+     */
+    public function __construct($latitude, $longitude)
+    {
+        $this->value = $latitude . ',' . $longitude;
+        $this->key = 'coordinates';
+    }
+}
