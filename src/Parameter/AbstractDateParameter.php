547705abcc2eba9a510ff1010a6d42059c0a114d
Bert Ramakers <bertramakers@me.com>
2021-01-12 12:23:55 +0100

    Remove protected method that was only used once

--- src/Parameter/AbstractDateParameter.php
+++ src/Parameter/AbstractDateParameter.php
@@ -5,22 +5,12 @@ namespace CultuurNet\SearchV3\Parameter;
 abstract class AbstractDateParameter extends AbstractParameter
 {
     /**
-     * Formats the date.
-     * @param $date
-     * @return string
-     */
-    protected function formatDate($date)
-    {
-        return $date->format(\DateTime::ATOM);
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function getValue()
     {
         if ($this->value instanceof \DateTime) {
-            return $this->formatDate($this->value);
+            return $this->value->format(\DateTime::ATOM);
         } else {
             return $this->value;
         }
